8c1861521078681b33f6616ca3683e1e48211ccd
Arjun Bhatt <arjun_239@hotmail.co.uk>
2017-07-13 18:36:19 -0700

    Update MMBTest2.java

--- MMBTesting/src/test/java/com/loveholidays/mmbtesting/MMBTest2.java
+++ MMBTesting/src/test/java/com/loveholidays/mmbtesting/MMBTest2.java
@@ -34,7 +34,7 @@ public class MMBTest2 {
     @BeforeTest
     public void chromeExtension()
     {
-     System.setProperty("webdriver.chrome.driver", "C:\\Users\\Arjun\\chromedriver\\chromedriver.exe"); 
+     System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
   
      driver=new ChromeDriver();
     }
